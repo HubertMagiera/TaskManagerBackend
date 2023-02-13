e9139e8b93cd1ae68853dcd7c3baac878ec4cbfc
HubertMagiera <125272288+HubertMagiera@users.noreply.github.com>
2023-02-13 20:23:49 +0100

    implemented classes based on db tables

---
+++ ToDoBackend/Entities/task.cs
@@ -0,0 +1,35 @@
+﻿namespace ToDoBackend.Entities
+{
+    public class task
+    {
+        //EF Core will call the parametrised ctor if it is provided
+        //if parametrised ctor is not provided, EF Core calls the parameterless ctor
+        public task(int task_id, string task_name, string? task_description, DateTime task_creation_date, DateTime? task_close_date, string task_priority, string task_status, task_type task_Type)
+        {
+            this.task_id = task_id;
+            this.task_name = task_name;
+            this.task_description = task_description;
+            this.task_creation_date = task_creation_date;
+            this.task_close_date = task_close_date;
+            this.task_priority = task_priority;
+            this.task_status = task_status;
+            this.task_Type = task_Type;
+        }
+
+        public int task_id { get; set; }
+
+        public string task_name { get; set; }
+
+        public string? task_description { get; set; }
+
+        public DateTime task_creation_date { get; set; }
+
+        public DateTime? task_close_date { get; set; }
+
+        public string task_priority { get; set; }
+
+        public string task_status { get; set; }
+
+        public task_type task_Type { get; set; }
+    }
+}
---
+++ ToDoBackend/Entities/user.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoBackend.Entities
+{
+    public class user
+    {
+        //EF Core will call the parametrised ctor if it is provided
+        //if parametrised ctor is not provided, EF Core calls the parameterless ctor
+        public user(int user_id, string user_name, string user_surname, string user_email, string user_password)
+        {
+            this.user_id = user_id;
+            this.user_name = user_name;
+            this.user_surname = user_surname;
+            this.user_email = user_email;
+            this.user_password = user_password;
+        }
+
+        [Required]
+        public int user_id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string user_name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string user_surname { get; set; }
+
+        [Required]
+        //verification if email address has correct format. In case it is invalid, returns a provided error message
+        [EmailAddress(ErrorMessage ="Please provide email address in correct format")]
+        [MaxLength(50)]
+        public string user_email { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string user_password { get; set; }
+    }
+}
